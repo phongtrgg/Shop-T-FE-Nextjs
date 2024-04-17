7aedd7d2aa36360947ebb91f30abcc305a1e4bf2
phongtrgg <trggphong@gmai.com>
2024-04-17 12:02:08 +0700

    edit copyright

--- shop/src/layouts/_copyright.tsx
+++ shop/src/layouts/_copyright.tsx
@@ -7,28 +7,27 @@ export default function Copyright({ className }: { className?: string }) {
   const currentYear = new Date().getFullYear();
   const { settings } = useSettings();
   return (
-    // <span className={cn('tracking-[0.2px]', className)}>
-    //   ©{currentYear}{' '}
-    //   <Link
-    //     className="text-heading font-medium hover:text-brand-dark"
-    //     href={settings?.siteLink ?? routes?.home}
-    //     target="_blank"
-    //   >
-    //     {settings?.siteTitle}
-    //   </Link>
-    //   . {settings?.copyrightText}{' '}
-    //   {settings?.externalText ? (
-    //     <Link
-    //       className="text-heading font-medium hover:text-brand-dark"
-    //       href={settings?.externalLink ?? routes?.home}
-    //       target="_blank"
-    //     >
-    //       {settings?.externalText}
-    //     </Link>
-    //   ) : (
-    //     ''
-    //   )}
-    // </span>
-    <></>
+    <span className={cn('tracking-[0.2px]', className)}>
+      ©{currentYear}{' '}
+      <Link
+        className="text-heading font-medium hover:text-brand-dark"
+        href={settings?.siteLink ?? routes?.home}
+        target="_blank"
+      >
+        {settings?.siteTitle}
+      </Link>
+      . {settings?.copyrightText}{' '}
+      {settings?.externalText ? (
+        <Link
+          className="text-heading font-medium hover:text-brand-dark"
+          href={settings?.externalLink ?? routes?.home}
+          target="_blank"
+        >
+          {settings?.externalText}
+        </Link>
+      ) : (
+        ''
+      )}
+    </span>
   );
 }
