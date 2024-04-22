74fbee63f7db4203fd068bb68a44e0aab04a7cfd
Truong-T-Phong <trggphong@gmail.com>
2024-04-22 17:42:51 +0700

    update env template

--- shop/src/pages/index.tsx
+++ shop/src/pages/index.tsx
@@ -112,10 +112,9 @@ const Home: NextPageWithLayout = () => {
   useEffect(() => {
     if (isLogin) {
       push('/');
+    } else {
+      window.location.href = process.env.NEXT_PUBLIC_APP_TOMIRU_URL as string;
     }
-    // else {
-    //   window.location.href = process.env.NEXT_PUBLIC_APP_TOMIRU_URL as string;
-    // }
   }, [isLogin]);
 
   return (
